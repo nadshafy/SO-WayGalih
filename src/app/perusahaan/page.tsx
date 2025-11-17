a8b34045a4e21f92d9617883bf21230b0fdcca11
Nadya <nadya.123140167@student.itera.ac.id>
2025-11-17 14:51:39 +0700

    debug

--- src/app/perusahaan/page.tsx
+++ src/app/perusahaan/page.tsx
@@ -43,6 +43,7 @@ export default function PerusahaanPage() {
         const cleanBase64 = base64.includes(",") ? base64.split(",")[1] : base64;
 
         dataObj[`${field}FileData`] = cleanBase64;
+        dataObj[`${field}FileName`] = file.name;
       }
     }
 
